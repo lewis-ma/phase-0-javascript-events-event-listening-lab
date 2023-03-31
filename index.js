e2476c0c71486ed1ebaee0a2f089d2bd2330b744
lewisky <lewisky28mawira@gmail.com>
2023-03-31 09:52:38 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-function addingEventListener() {
+const button = document.getElementById('myButton');
+const paragraph = document.getElementById('myParagraph');
+
+function changeText() {
+  paragraph.textContent = 'New text!';
 }
+
+button.addEventListener('click', changeText);
